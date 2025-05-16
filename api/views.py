5edd573a7300196237373aa23793eee94f67607d
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-16 23:07:53 +0300

    Implement user creation logic and add update/delete functionality for blog posts

--- api/views.py
+++ api/views.py
@@ -14,8 +14,12 @@ def get_users(request):
 @api_view(['POST'])
 def create_user(request):
     data = request.data
-    # Here you would typically save the user to the database
-    return Response({"message": "User created successfully", "data": data})
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message": "User created successfully", "data": serializer.data}, status=201)
+    else:
+        return Response({"error": serializer.errors}, status=400)
 
 @api_view(['GET'])
 def get_blog_posts(request):
@@ -32,3 +36,22 @@ def create_blog_post(request):
         return Response({"message": "Blog post created successfully", "data": serializer.data}, status=201)
     else:
         return Response({"error": serializer.errors}, status=400)
+
+@api_view(['PUT', 'DELETE'])
+def update_blog_post(request, pk):
+    try:
+        blog_post = BlogPost.objects.get(pk=pk)
+    except BlogPost.DoesNotExist:
+        return Response({"error": "Blog post not found"}, status=404)
+
+    if request.method == 'PUT':
+        serializer = BlogPostSerializer(blog_post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Blog post updated successfully", "data": serializer.data}, status=200)
+        else:
+            return Response({"error": serializer.errors}, status=400)
+
+    elif request.method == 'DELETE':
+        blog_post.delete()
+        return Response({"message": "Blog post deleted successfully"}, status=204)
