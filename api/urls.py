ec6636affbb90e90b7a9a133e1cd5060c815f4ca
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-16 23:08:02 +0300

    Add URL patterns for updating and deleting blog posts

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from .views import get_users, create_user, get_blog_posts, create_blog_post
+from .views import get_users, create_user, get_blog_posts, create_blog_post, update_blog_post
 from django.urls import path
 
 urlpatterns=  [
@@ -6,4 +6,6 @@ urlpatterns=  [
     path('users/create/', create_user, name='create_user'),
     path('blogposts/', get_blog_posts, name='get_blog_posts'),
     path('blogposts/create/', create_blog_post, name='create_blog_post'),
+    path('blogposts/<int:pk>/', update_blog_post, name='update_blog_post'),
+    path('blogposts/<int:pk>/delete/', update_blog_post, name='delete_blog_post'),
 ]
