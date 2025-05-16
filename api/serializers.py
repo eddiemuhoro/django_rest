802f7c35c7483ff76850a765f897ee4f052bf6b7
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-16 22:26:40 +0300

    Refactor UserSerializer to improve code clarity and maintainability

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import User  # Assuming you have a User model in models.py
+from .models import User, BlogPost  # Assuming you have a User model in models.py
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'  # or specify the fields you want to include
+        fields = '__all__' 
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = '__all__' 
