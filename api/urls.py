5e0270e001b2ddcfa06bae689b40412b9a09a758
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-16 22:47:10 +0300

    Add blog post endpoints to the API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from .views import get_users, create_user
+from .views import get_users, create_user, get_blog_posts, create_blog_post
 from django.urls import path
 
 urlpatterns=  [
     path('users/', get_users, name='get_user'),
     path('users/create/', create_user, name='create_user'),
+    path('blogposts/', get_blog_posts, name='get_blog_posts'),
+    path('blogposts/create/', create_blog_post, name='create_blog_post'),
 ]
